0047018e8f79ed5eb82544105140e0866389067b
DESKTOP-BD06TMI\hp <armandopg12022000@gmail.com>
2024-04-02 19:18:38 +0200

    [ADD] creacion portada.js

--- js/portada.js
+++ js/portada.js
@@ -1,1 +1,14 @@
-//archivo js
+document.addEventListener("DOMContentLoaded", function() {
+    var registroLink = document.querySelector('.option[href="registro.html"]');
+    var loginLink = document.querySelector('.option[href="login.html"]');
+
+    registroLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        window.location.href = "registro.html";
+    });
+
+    loginLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        window.location.href = "login.html";
+    });
+});
