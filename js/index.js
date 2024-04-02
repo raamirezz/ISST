1608f593ac43f34c59a715e40b783d79452e336e
DESKTOP-BD06TMI\hp <armandopg12022000@gmail.com>
2024-04-02 19:18:05 +0200

    [MOD] correccíon nombres

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// Lista de las imágenes de las urbanizaciones
+var imagenesUrbanizaciones = [
+    "../content/Urbanización1.jpeg",
+    "../content/Urbanización2.jpeg",
+    "../content/Urbanización3.jpg",
+    "../content/Urbanización4.jpg"
+];
+
+// Función para cambiar la imagen cada 5 segundos
+var indexImagen = 0; // Índice de la imagen actual
+function cambiarImagen() {
+    document.getElementById("urbanizacion").src = imagenesUrbanizaciones[indexImagen];
+    indexImagen = (indexImagen + 1) % imagenesUrbanizaciones.length; // Avanzar al siguiente índice
+}
+
+// Cambiar la imagen inicial
+cambiarImagen();
+
+// Cambiar la imagen cada 5 segundos
+setInterval(cambiarImagen, 5000); // 5000 milisegundos = 5 segundos
